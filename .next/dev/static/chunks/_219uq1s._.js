97b5de50a876f85349b7eac99ad29ffd3ff0b0d1
Joshndeule <joshndeule@gmail.com>
2026-08-12 09:02:42 +0200

    update

---
+++ .next/dev/static/chunks/_219uq1s._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[root-of-the-server]__1dz8k9u._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/src_0k5ne47._.js","static/chunks/node_modules_next_0ig31tq._.js","static/chunks/node_modules_motion-dom_dist_es_0p27dta._.js","static/chunks/node_modules_framer-motion_dist_es_0atxf2l._.js","static/chunks/node_modules_three_build_three_core_18d0oq6.js","static/chunks/node_modules_three_build_three_module_03rsyo6.js","static/chunks/node_modules_three_build_three_module_1_wgwg8.js","static/chunks/node_modules_@react-three_fiber_dist_0e3k4b4._.js","static/chunks/node_modules_0ri99a7._.js"],
+    source: "entry"
+});
